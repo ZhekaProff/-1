1589997a5c87a5ca73b72f7e9cc84a8a81e1b4d1
«Zheka <zhekakad@ya.ru>
2022-04-25 17:38:37 +0900

    рещение задачи 3

---
+++ lesson_003/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число a: ");
+int a =  int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b =  int.Parse(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c =  int.Parse(Console.ReadLine());
+int max = a;
+Console.Write("Максимальное число: ");
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
