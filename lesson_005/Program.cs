4e717ba82a266b71d13aaaa0399e0214afafef9c
«Zheka <zhekakad@ya.ru>
2022-04-25 18:32:00 +0900

    решение задачи про деление нацело

---
+++ lesson_005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите 3-х значное число: ");
+int a =  int.Parse(Console.ReadLine());
+if (a > 99 && a < 1000)
+{
+Console.Write("Последяя цифра 3-х значного числа = ");
+Console.WriteLine(a % 10);
+}
+else
+{
+    Console.WriteLine("Число не 3-х значное!");
+}
